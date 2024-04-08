e323e38c450d8a037d08e8470d2beeea33d95a31
Russell <russell2259@tuta.io>
2024-04-08 14:54:32 -0600

    Update sw.js

--- stuff/sw.js
+++ stuff/sw.js
@@ -1,10 +1,3 @@
-/*global UVServiceWorker,__uv$config*/
-/*
- * Stock service worker script.
- * Users can provide their own sw.js if they need to extend the functionality of the service worker.
- * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
- * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
- */
 importScripts('localforage.js');
 
 localforage.config({
@@ -14,8 +7,10 @@ localforage.config({
   storeName: 'ultraviolet_config'
 });
 
-const bareServer = await localForage.getItem('bare');
-if (bareServer) self.__uv$config.bare === bareServer;
+try {
+  const bareServer = await localforage.getItem('bare');
+  if (bareServer) self.__uv$config.bare === bareServer;
+} catch {}
 
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
