5a62511d72be605c8f5314e317ed1b1c2f64a3ce
Russell <russell2259@tuta.io>
2024-04-08 14:34:40 -0600

    Update sw.js

--- stuff/sw.js
+++ stuff/sw.js
@@ -5,9 +5,15 @@
  * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
+importScripts('localforage.js');
 importScripts('uv.bundle.js');
 importScripts('uv.config.js');
 importScripts(__uv$config.sw || 'uv.sw.js');
+
+localforage.setDriver(localforage.INDEXEDDB);
+
+const bareServer = await localForage.getItem('bare');
+if (bareServer) self.__uv$config.bare === bareServer;
 
 const sw = new UVServiceWorker();
 
